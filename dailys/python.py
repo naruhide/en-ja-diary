d9cb213c4c4002507759407479075969424b6815
naruhide.ueda <neru@naruhide.com>
2021-09-22 14:29:55 +0900

    Rename function

--- dailys/python.py
+++ dailys/python.py
@@ -7,8 +7,11 @@ import subprocess
 import webbrowser
 
 
-def auto_browse():
-    """"""
+def auto_browsing():
+    """
+    Enter the url you want to open in the variable urls.
+    The default browser registered on your PC will be launched.
+    """
 
     urls = (
         'https://github.com/naruhide/my-util',
@@ -18,8 +21,9 @@ def auto_browse():
         webbrowser.open(url)
 
 
-def launch_app():
+def launch_apps():
     """" Enter the full path of the app you want to launch in the variable app_absolute_paths """
+
     app_absolute_paths = (
         "/usr/bin/gedit",
     )
@@ -30,8 +34,9 @@ def launch_app():
 
 def main():
     """ Adjust the function you want to use with comments. """
-    auto_browse()
-    # launch_app()
+
+    auto_browsing()
+    # launch_apps()
 
 
 if __name__ == "__main__":
