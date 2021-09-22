8204726900a5038ec992c944c32e26d657f36e83
naruhide.ueda <neru@naruhide.com>
2021-09-22 11:38:53 +0900

    Create python.py

--- dailys/python.py
+++ dailys/python.py
@@ -3,16 +3,38 @@
 
 """
 
-import ...
-...
+import os.path
+import sys
+import subprocess
+import webbrowser
 
-def a():
-    """doc"""
-    pass
+def auto_app():
+    dirname = os.path.dirname(__file__)
+    app_file = os.path.join(dirname, "config", "app.txt")  # appfile is here.
+    try:
+        with open(app_file, "r", encoding="utf8") as f:
+            path_with_line_breaks = f.readlines()
+            app_paths = list(map(lambda s: s.rstrip("\n"), path_with_line_breaks))
+        for app in app_paths:
+            subprocess.Popen(app)
+    except FileNotFoundError as err:
+        print(err)
+        sys.exit()
 
-def b():
-    pass
+def browser():
+    dirname = os.path.dirname(__file__)
+    url_file = os.path.join(dirname, "config", "url.txt")  # urlfile is here.
+    try:
+        with open(url_file, "r", encoding="utf8") as f:
+            urls_with_line_breaks = f.readlines()
+            urls = list(map(lambda s: s.rstrip("\n"), urls_with_line_breaks))
+    except FileNotFoundError as err:
+        print(err)
+        sys.exit()
+
+    for url in urls:
+        webbrowser.open(url)
 
 if '__name__' == '__main__':
-    # a()
-    b()
+    auto_app()
+    browser()
