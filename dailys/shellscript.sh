0f8f3495525f0113116df0d555146058adac053c
naruhide.ueda <neru@naruhide.com>
2021-09-22 11:26:51 +0900

    Create function main

--- dailys/shellscript.sh
+++ dailys/shellscript.sh
@@ -1,26 +1,36 @@
 #!/bin/bash -vx
 
-function apt_system_upgrade () {
-  echo -e "\n$(date "+%d-%m-%Y --- %T") --- Starting work\n"
-  sudo apt-get update
-  sudo apt-get -y upgrade
-  sudo apt-get -y autoremove
-  sudo apt-get autoclean
-  echo -e "\n$(date "+%T") \t Script Terminated"
+function apt_system_maintenance () {
+    echo -e "\n$(date "+%d-%m-%Y --- %T") --- Starting work\n"
+    sudo apt-get update
+    sudo apt-get -y upgrade
+    sudo apt-get -y autoremove
+    sudo apt-get autoclean
+    echo -e "\n$(date "+%T") \t Script Terminated"
 }
 
 function git_push () {
-  git add .
-  read -p 'Enter commit message: ' COMMIT_MESSAGE
-  git commit -m "$COMMIT_MESSAGE"
-  expect -c "
-  spawn git push -u origin master
-  expect \"Username\" ; send \"$GIT_USERNAME\n\"
-  expect \"Password\" ; send \"$GIT_ACCESS_TOKEN\n\"
-  expect \"$\"
-  exit 0
-  "
+    # Prerequisite:
+    #     echo 'export GIT_USERNAME="your_github_username"' >> ~/.bashrc
+    #     echo 'export GIT_ACCESS_TOKEN="your_github_access_token"' >> ~/.bashrc
+
+    git add .
+    read -p 'Enter commit message: ' COMMIT_MESSAGE
+    git commit -m "$COMMIT_MESSAGE"
+    expect -c "
+    spawn git push -u origin master
+    expect \"Username\" ; send \"$GIT_USERNAME\n\"
+    expect \"Password\" ; send \"$GIT_ACCESS_TOKEN\n\"
+    expect \"$\"
+    exit 0
+    "
 }
 
-# apt_system_upgrade
-git_push
+function main () {
+    ### Please adjust the function you want to use with comments. ###
+
+    # apt_system_maintenance
+    git_push
+}
+
+main
