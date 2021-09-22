423f27ee935a9c8186e88cbe38b483c5c0b84ad0
naruhide.ueda <neru@naruhide.com>
2021-09-22 12:10:17 +0900

    Reformat __name__

--- dailys/python.py
+++ dailys/python.py
@@ -4,24 +4,12 @@
 """
 
 import os.path
-import sys
 import subprocess
+import sys
 import webbrowser
 
-def auto_app():
-    dirname = os.path.dirname(__file__)
-    app_file = os.path.join(dirname, "config", "app.txt")  # appfile is here.
-    try:
-        with open(app_file, "r", encoding="utf8") as f:
-            path_with_line_breaks = f.readlines()
-            app_paths = list(map(lambda s: s.rstrip("\n"), path_with_line_breaks))
-        for app in app_paths:
-            subprocess.Popen(app)
-    except FileNotFoundError as err:
-        print(err)
-        sys.exit()
 
-def browser():
+def auto_browser():
     dirname = os.path.dirname(__file__)
     url_file = os.path.join(dirname, "config", "url.txt")  # urlfile is here.
     try:
@@ -35,6 +23,28 @@ def browser():
     for url in urls:
         webbrowser.open(url)
 
-if '__name__' == '__main__':
-    auto_app()
-    browser()
+
+def launch_app():
+    dirname = os.path.dirname(__file__)
+    app_file = os.path.join(dirname, "config", "app.txt")  # appfile is here.
+    try:
+        with open(app_file, "r", encoding="utf8") as f:
+            path_with_line_breaks = f.readlines()
+            app_paths = list(map(lambda s: s.rstrip("\n"), path_with_line_breaks))
+        for app in app_paths:
+            subprocess.Popen(app)
+    except FileNotFoundError as err:
+        print(err)
+        sys.exit()
+
+
+def main():
+    """ Please adjust the function you want to use with comments. """
+
+    # auto_browse()
+    # launch_app()
+    print('Finish')
+
+
+if __name__ == "__main__":
+    main()
