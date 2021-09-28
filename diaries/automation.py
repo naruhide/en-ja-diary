769bb7ea8a2c7e522ee376548bb8505fb0ccabae
naruhide.ueda <neru@naruhide.com>
2021-09-28 12:02:25 +0900

    Rename function names

--- diaries/automation.py
+++ diaries/automation.py
@@ -11,14 +11,68 @@ import argparse
 import os
 import re
 import subprocess
+import sys
 import webbrowser
 from zipfile import is_zipfile
 from zipfile import ZipFile
 
-from github import Github
+try:
+    from github import Github
+except ModuleNotFoundError:
+    print('If you want to use the readme_update function, please install the PyGithub module.')
 
 
-def add_summary_to_readme():
+def auto_browsing():
+    """
+    Purpose:
+        Open all the usual pages together.
+    Usage:
+        Enter the url you want to open in the variable urls.
+        The default browser registered on your PC will be launched.
+    """
+
+    urls = (
+        'https://translate.google.com/?hl=ja&sl=ja&tl=en&op=translate',
+        'https://renso-ruigo.com/',
+        'https://eowf.alc.co.jp/',
+        'https://muumuu-mail.com/login',
+        'https://github.com/naruhide/en-ja-diary/tree/master/diaries',
+        'https://docs.python.org/ja/3/library/index.html',
+        'https://wiki.python.org/moin/UsefulModules',
+        'https://docs.ansible.com/ansible/2.9_ja/#',
+    )
+
+    for url in urls:
+        webbrowser.open(url)
+
+
+def automatic_launch_of_apps():
+    """
+    Purpose:
+        Launch apps in bulk.
+    Usage:
+        Enter the full path of the app you want to launch in the variable app_absolute_paths.
+        If you pass an argument at startup, write it in the tuple with an absolute path as well.
+    Example:
+        ('/usr/bin/gedit', '/home/testuser/doc/test.txt')  # Pass arguments to gedit.
+        ('ls', '-la')                                      # Pass options to ls command.
+        ls                                                 # Tuples are not needed if there are no arguments or options.
+    """
+
+    app_absolute_paths = (
+        ('/usr/bin/gedit', '/home/naruhide/Desktop/Memos/diary1.txt'),
+        '/snap/bin/pycharm-community',
+    )
+
+    for app in app_absolute_paths:
+        try:
+            subprocess.Popen(app)
+        except FileNotFoundError as err:
+            print(err)
+            continue
+
+
+def readme_update():
     """
     Purpose:
         Automatically update the README.md.
@@ -92,67 +146,17 @@ def add_summary_to_readme():
     repo.create_file('README.md', 'Update README.md', full_text, branch='master')
 
 
-def auto_browsing():
-    """
-    Purpose:
-        Open all the usual pages together.
-    Usage:
-        Enter the url you want to open in the variable urls.
-        The default browser registered on your PC will be launched.
-    """
-
-    urls = (
-        'https://translate.google.com/?hl=ja&sl=ja&tl=en&op=translate',
-        'https://renso-ruigo.com/',
-        'https://eowf.alc.co.jp/',
-        'https://muumuu-mail.com/login',
-        'https://github.com/naruhide/en-ja-diary/tree/master/diaries',
-        'https://docs.python.org/ja/3/library/index.html',
-        'https://wiki.python.org/moin/UsefulModules',
-        'https://docs.ansible.com/ansible/2.9_ja/#',
-    )
-
-    for url in urls:
-        webbrowser.open(url)
-
-
-def launch_apps():
-    """
-    Purpose:
-        Launch apps in bulk.
-    Usage:
-        Enter the full path of the app you want to launch in the variable app_absolute_paths.
-        If you pass an argument at startup, write it in the tuple with an absolute path as well.
-    Example:
-        ('/usr/bin/gedit', '/home/testuser/doc/test.txt') # Pass arguments to gedit.
-        ('ls', '-la')                                     # Pass options to ls command.
-        ls                                                # Tuples are not needed if there are no arguments or options.
-    """
-
-    app_absolute_paths = (
-        ('/usr/bin/gedit', '/home/naruhide/Desktop/Memos/diary1.txt'),
-        '/snap/bin/pycharm-community',
-    )
-
-    for app in app_absolute_paths:
-        try:
-            subprocess.Popen(app)
-        except FileNotFoundError as err:
-            print(err)
-            continue
-
-
 def unzip():
     """
     Purpose:
         Unzip the zip file to the same hierarchy where you ran this python file.
     Usage:
         Enter the following command on the command line to display the help page.
-            python3 python.py -h
+            python3 automation.py -h
     Example:
-        python3 python.py --zipfile test1.zip test2.zip
-        python3 python.py --zipfile /home/testuser/doc/test1.zip
-        python3 python.py --zipfile ~/test1.zip /home/testuser/test2.zip
+        python3 automation.py --zipfile test1.zip test2.zip
+        python3 automation.py --zipfile /home/testuser/doc/test1.zip
+        python3 automation.py --zipfile ~/test1.zip /home/testuser/test2.zip
 
     """
     # TODO: Function addition
@@ -185,9 +189,9 @@ def main():
         Adjust the function you want to use with comments.
     """
 
-    # add_summary_to_readme()
     auto_browsing()
-    # launch_apps()
+    # automatic_launch_of_apps()
+    # readme_update()
     # unzip()
 
 
